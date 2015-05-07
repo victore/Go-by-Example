c85fb42fba414e59f4287e2a356342d3bba3631e
Victor Guadarrama <victor.guadarrama3@gmail.com>
2015-05-06 19:51:56 -0500

    11-12

---
+++ 11-Slices.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  /* Unlike arrays, slices are typed only by the elements they contain
+  (not the number of elements).*/
+  // To create an empty slice with non-zero length, use the bultin make.
+  s := make([]string, 3)
+  fmt.Println("emp:", s)
+
+  s[0] = "a"
+  s[1] = "b"
+  s[2] = "c"
+  fmt.Println("set:", s)
+  fmt.Println("get:", s[2])
+
+  fmt.Println("len:", len(s))
+
+  s = append(s, "d")
+  s = append(s, "e", "f")
+  fmt.Println("apd:", s)
+
+
+  c := make([]string, len(s))
+  copy(c, s)
+  fmt.Println("cpy:", c)
+
+  // Slices support a “slice” operator with the syntax slice[low:high].
+  l := s[2:5]
+  fmt.Println("sl1:", l)
+
+  //Hasta antes del 5to element
+  l = s[:5]
+  fmt.Println("sl2:", l)
+
+  //A partir del elemento No. 2
+  l = s[2:]
+  fmt.Println("sl3:", l)
+
+  // We can declare and initialize a variable for slice in a single line as well.
+  t := []string{"g", "h", "i"}
+  fmt.Println("dcl:", t)
+
+  // Slices can be composed into multi-dimensional data structures.
+  twoD := make([][]int, 3)
+  for i := 0; i < 3; i++ {
+    innerLen := i + 1
+    twoD[i] = make([]int, innerLen)
+    for j := 0; j < innerLen; j++ {
+      twoD[i][j] = i + j
+    }
+  }
+  fmt.Println("2d: ", twoD)
+}
---
+++ 12-Maps.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  /* To create an empty map,
+  use the builtin make: make(map[key-type]val-type).*/
+  m := make(map[string]int)
+
+  // Set key/value pairs using typical name[key] = val syntax.,
+  m["k1"] = 7
+  m["k2"] = 13
+
+  fmt.Println("map:", m)
+
+  v1 := m["k1"]
+  fmt.Println("v1: ", v1)
+
+  fmt.Println("len:", len(m))
+
+  delete(m, "k2")
+  fmt.Println("map:", m)
+  
+  _, prs := m["k2"]
+  fmt.Println("prs:", prs)
+
+  n := map[string]int{"foo": 1, "bar": 2}
+  fmt.Println("map:", n)
+}
